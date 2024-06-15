680ee092ceed703d780e05e52a3cc3d1bffa11b6
SourabhPatil <202251095@iiitvadodara.ac.in>
2024-06-15 23:43:31 +0530

    usePopcoren Part1

--- 07-usepopcorn/usepopcorn/src/main.jsx
+++ 07-usepopcorn/usepopcorn/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from './StarRating.jsx'
+
+function Test(){
+  const [movieRating,setMovieRating]=useState(0);
+  return(
+    <div>
+      <StarRating color='blue'maxRating={10} onSetRating={setMovieRating}/>
+      <p>The movie was rated {movieRating} star by user</p>
+    </div>
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating/>
+    <Test/>
   </React.StrictMode>,
 )
