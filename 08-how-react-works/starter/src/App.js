c63cc9407ceef07f757157f37c1ada02f4245bb7
SourabhPatil <202251095@iiitvadodara.ac.in>
2024-06-17 01:26:11 +0530

    Completed React BTS

--- 08-how-react-works/starter/src/App.js
+++ 08-how-react-works/starter/src/App.js
@@ -29,6 +29,8 @@ export default function App() {
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
+
+
   return (
     <div>
       <div className="tabs">
@@ -62,8 +64,24 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("Render")
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  function handleTripleInc(){
+    setLikes((likes)=>likes + 1);
+    setLikes((likes)=>likes + 1);
+    setLikes((likes)=>likes + 1);
+  }
+
+  function handleUndo(){
+    setLikes(0);
+    setShowDetails(true)
+  }
+
+  function handleUndoLater(){
+    setTimeout(handleUndo,2000);
   }
 
   return (
@@ -79,13 +97,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
