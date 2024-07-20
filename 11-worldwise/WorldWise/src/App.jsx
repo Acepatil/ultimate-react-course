939db252d90a128e277be96487cf6f226273c612
SourabhPatil <202251095@iiitvadodara.ac.in>
2024-07-21 01:26:07 +0530

    "Redux Intro Complete"

--- 11-worldwise/WorldWise/src/App.jsx
+++ 11-worldwise/WorldWise/src/App.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-inner-declarations */
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import { lazy, Suspense } from "react"
-import { CitiesProvider } from "./context/CitiesContext"
-import { AuthProvider } from "./context/FakeAuthContext"
-import ProtectedRoute from "./pages/ProtectedRoute"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { CitiesProvider } from "./context/CitiesContext";
+import { AuthProvider } from "./context/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 // import Product from"./pages/Product"
 // import Pricing from"./pages/Pricing"
@@ -12,56 +12,53 @@ import ProtectedRoute from "./pages/ProtectedRoute"
 // import PageNotFound from"./pages/PageNotFound"
 // import Login from "./pages/Login"
 
-const HomePage=lazy(()=>import ("./pages/HomePage"))
-const Product=lazy(()=>import ("./pages/Product"))
-const Pricing=lazy(()=>import ("./pages/Pricing"))
-const AppLayout=lazy(()=>import ("./pages/AppLayout"))
-const PageNotFound=lazy(()=>import ("./pages/PageNotFound"))
-const Login=lazy(()=>import ("./pages/Login"))
+const HomePage = lazy(() => import("./pages/HomePage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Login = lazy(() => import("./pages/Login"));
 
-import CityList from "./components/CityList"
-import CountryList from "./components/CountryList"
-import City from "./components/City"
-import Form from'./components/Form'
-import SpinnerFullPage from './components/SpinnerFullPage'
-
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
 function App() {
-  
   return (
     <AuthProvider>
-    <CitiesProvider>
-     
-    <BrowserRouter>
-    <Suspense fallback={<SpinnerFullPage/>}>
-    <Routes>
-      <Route path="/product"element={<Product/>}></Route>
-      <Route index element={<HomePage/>}></Route>
-      
-      <Route path="/login"element={<Login/>}></Route>
-        
-      <Route path="/app"element={
-        <ProtectedRoute>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/product" element={<Product />}></Route>
+              <Route index element={<HomePage />}></Route>
 
-        <AppLayout/>
-        </ProtectedRoute>
-        }>
-      <Route index element={<Navigate replace to="cities"/>}/>
-      <Route path="cities" element={<CityList />}/>
-      <Route path="cities/:id" element={<City/>}/>
-      <Route path="countries" element={<CountryList />}/>
-      <Route path="form" element={<Form/>}/>
-      </Route>
-      <Route path="/pricing"element={<Pricing/>}></Route>
-      <Route path="*"element={<PageNotFound/>}></Route>
-      
-    </Routes>
-    </Suspense>
-    </BrowserRouter>
-    
-    </CitiesProvider>
+              <Route path="/login" element={<Login />}></Route>
+
+              <Route
+                path="/app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="/pricing" element={<Pricing />}></Route>
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 15-redux-intro/Redux/src/store-v2.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import accountReducer from "./features/account/accountSlice";
+import customerReducer from "./features/customer/customerSlice";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import { thunk } from "redux-thunk";
+
+const rootReducer=combineReducers({
+    account:accountReducer,
+    customer:customerReducer
+})
+const store = createStore(rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)));
+
+export default store
+
